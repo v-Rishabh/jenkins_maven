80083fa7e200370da3ee6cb654b7a89cf9dad22b
Rishabh Verma <rishabhverma17@gmail.com>
2018-11-17 08:37:18 +0530

    Changes in App.java

--- src/main/java/com/verma/rishabh/App.java
+++ src/main/java/com/verma/rishabh/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Jenkins" );
+        System.out.println( "Welcome to Jenkins!" );
         Person p = new Person("Rishabh","Verma");
         String fullName = p.getFullName();
         System.out.println("Full Name is : "+fullName);
